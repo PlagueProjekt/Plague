de773257cfa14f8c385b38d53fb4724707b1f877
PlagueProjekt <projekt.plague@gmail.com>
2017-03-13 20:02:17 +0100

    Added IBehavior
    Initially added IBehavior
    -> which Params should be passed?

---
+++ Level0/Level0/Assets/src/IBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBehavior
+{
+    States getState();
+
+    // TODO: think about params
+    void executeBehavior();
+    States calcNewState();
+	
+}
